12cda0ff4599709f6eb4877037df698988d45f3f
AdityaAnandCodes <adityaanandatwork276@gmail.com>
2024-12-12 23:23:41 +0530

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="bg-black text-white py-4 mt-20">
+        <div className="container mx-auto text-center">
+          <p className="text-sm">© 2024 M-Arcade. All rights reserved.</p>
+          <p className="text-sm">Powered by Web3 and Blockchain Technology</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
